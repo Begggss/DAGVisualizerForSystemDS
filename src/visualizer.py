a18fa9f8deb90aa26bc32ed357dcf93f7f36b009
begum_94 <parlacajias@campus.tu-berlin.de>
2023-09-28 14:23:17 +0200

    added line numbers to sankey diagram

--- src/visualizer.py
+++ src/visualizer.py
@@ -18,7 +18,7 @@ def visualize_tree(path):
 
 
 def visualize_sankey(path):
-    labels, source, target, value = blockParser.tree_to_sankey(path)
+    labels, source, target, value, linenumbers = blockParser.tree_to_sankey(path)
     title =  path.split('/').pop()
     fig = go.Figure(go.Sankey(
         arrangement='freeform',
@@ -27,40 +27,76 @@ def visualize_sankey(path):
             thickness=30,
             line=dict(color="black", width=0.5),
             label= labels,
+            customdata=linenumbers,
+            hovertemplate='%{customdata}<extra></extra>'
         ),
         link=dict(
             arrowlen=15,
             source=source,
             target=target,
-            value=value
+            value=value,
+            hovertemplate='Link to %{target.customdata}<extra></extra>'
         )))
 
-    fig.update_layout(hovermode=False,title_text= title, font_size=15)
+    fig.update_layout(title_text= title, font_size=15)                       #Comment out this line and uncomment line 41 to turn off hoverlabel
+    #fig.update_layout(title_text=title, font_size=15, hovermode = False)
     fig.show()
+
+
+# def visualize_operations(path, node):
+#     labels, source, target, value, hover_label = blockParser.create_sankey_nodes(path, node)
+#     fig = go.Figure(go.Sankey(
+#         arrangement='freeform',
+#         node=dict(
+#             pad=100,
+#             thickness=30,
+#             line=dict(color="black", width=0.5),
+#             label=labels,
+#             customdata = hover_label,
+#             hovertemplate='%{customdata}<extra></extra>',
+#
+#         ),
+#         link=dict(
+#             arrowlen=50,
+#             source=source,
+#             target=target,
+#             value=value,
+#             hovertemplate = 'Link from  %{source.customdata}<br />' +
+#                     'to %{target.customdata}<br />' +
+#                     '<br /><extra></extra>',
+#         )))
+#
+#     fig.update_layout(title_text= node, font_size=15)
+#     fig.show()
+
 
 
 def visualize_operations(path, node):
-    labels, source, target, value, hover_label = blockParser.create_sankey_nodes(path, node)
-    fig = go.Figure(go.Sankey(
-        arrangement='freeform',
-        node=dict(
-            pad=100,
-            thickness=30,
-            line=dict(color="black", width=0.5),
-            label=labels,
-            customdata = hover_label,
-            hovertemplate='%{customdata}<extra></extra>',
+    if node == None or node == " ":
+        print("Please give the name of the node you would like to visualize")
+    else:
+        labels, source, target, value, hover_label = blockParser.create_sankey_nodes(path, node)
+        fig = go.Figure(go.Sankey(
+            arrangement='freeform',
+            node=dict(
+                pad=100,
+                thickness=30,
+                line=dict(color="black", width=0.5),
+                label=labels,
+                customdata=hover_label,
+                hovertemplate='%{customdata}<extra></extra>',
 
-        ),
-        link=dict(
-            arrowlen=50,
-            source=source,
-            target=target,
-            value=value,
-            hovertemplate = 'Link from  %{source.customdata}<br />' +
-                    'to %{target.customdata}<br />' +
-                    '<br /><extra></extra>',
-        )))
+            ),
+            link=dict(
+                arrowlen=50,
+                source=source,
+                target=target,
+                value=value,
+                hovertemplate='Link from  %{source.customdata}<br />' +
+                              'to %{target.customdata}<br />' +
+                              '<br /><extra></extra>',
+            )))
 
-    fig.update_layout(title_text= node, font_size=15)
-    fig.show()
+        fig.update_layout(title_text=node, font_size=15)
+        fig.show()
+
